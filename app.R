7f2870ea5e029be6d5ba11260221f8e9972067a1
gstorrez <147574935+gstorrez@users.noreply.github.com>
2023-12-06 20:59:41 -0700

    Add files via upload
    Multilinear regression model

--- app.R
+++ app.R
@@ -1,71 +1,100 @@
-# Linear Regression Model
+# MultiLinear Regression Model
 
 library(shiny)
 library(shinydashboard)
+library(shinythemes)
 library(ggplot2)
 library(dplyr)
 library(stringr)
 library(readxl)
 library(tidyr)
+library(tidymodels)
 
-# Load data
 Amazon_Sale_Report <- read_excel("~/Greg's School/ISTA 498/Amazon Sale Report.xlsx")
 ASR <- Amazon_Sale_Report
 
-ASR$`promotion_ids` <- str_replace_all(ASR$`promotion_ids`, 'Amazon PLCC Free-Financing Universal Merchant AAT','Yes')
-ASR$`promotion_ids` <- str_replace_all(ASR$`promotion_ids`, 'IN Core Free Shipping','Yes')
-ASR$`promotion_ids` <- str_replace_all(ASR$`promotion_ids`, 'VPC-44571 Coupon','Yes')
-
-m1 <- lm(Amount ~ discounted_price + rating, data = ASR)
-
-# UI
 ui <- dashboardPage(
-  dashboardHeader(title = "Template for Variables"),
+  dashboardHeader(title = "Multilinear Regression Model" ,titleWidth = 300),
   dashboardSidebar(
-    selectInput("variable", "Variable", choices = c("discounted_price", "rating","rating_count","Qty","Savings" )),
-    actionButton("update", "Update Plot")
+    selectInput("variable1", "Variable 1", choices = colnames(ASR)),
+    selectInput("variable2", "Variable 2", choices = colnames(ASR)),
+    selectInput("variable3", "Variable 3", choices = colnames(ASR)),
+    actionButton("update", "Update Model")
   ),
   dashboardBody(
     fluidRow(
       box(
-        title = "Test",
+        title = "Sales Forecast Analysis Model",
         status = "primary",
         solidHeader = TRUE,
         plotOutput("salesPlot")
+      ),
+      box(
+        title = "P-Values and Variance",
+        status = "primary",
+        solidHeader = TRUE,
+        textOutput("pValuesOutput"),
+        textOutput("varianceOutput"),
+        textOutput("pValueMessage"),
+        textOutput("varianceMessage")
       )
     )
   )
 )
 
-# Server
 server <- function(input, output, session) {
   
   filtered_data <- reactive({
     ASR %>%
-      select("Amount", input$variable)
+      select("Amount", input$variable1, input$variable2, input$variable3)
   })
   
   observeEvent(input$update, {
-  
-    updated_model <- lm(formula = as.formula(paste("Amount ~", input$variable)), data = ASR)
-    
- 
-    x_val <- seq(from = min(ASR[[input$variable]]), to = max(ASR[[input$variable]]), length.out = nrow(ASR))
-    
-
-    y_pred <- predict(updated_model, newdata = data.frame(filtered_data()))
-    
-    # Plot
     output$salesPlot <- renderPlot({
-      ggplot(ASR, aes_string(x = input$variable, y = "Amount")) +
+      
+      model <- lm(Amount ~ ., data = filtered_data())
+      
+      predictions <- predict(model, filtered_data())
+      
+      ggplot(filtered_data(), aes(x = predictions, y = Amount)) +
         geom_point() +
-        geom_line(aes(x = x_val, y = y_pred), color = "blue") +
-        labs(title = paste("Sales Performance vs.", input$variable),
-             x = input$variable, y = "Amount")
+        geom_smooth(method = "lm", se = FALSE, color = "red") +
+        labs(title = "Multilinear Regression Plot",
+             x = "Predicted Amount",
+             y = "Actual Amount")
+    })
+    
+  
+    summary_data <- summary(lm(Amount ~ ., data = filtered_data()))
+    p_values <- summary_data$coefficients[, 4]
+    variance <- summary_data$sigma
+    
+    output$pValuesOutput <- renderText({
+      paste("P-values: ", paste(round(p_values, 4), collapse = ", "))
+    })
+    
+    output$varianceOutput <- renderText({
+      paste("Variance: ", round(variance, 4))
+    })
+    
+  
+    output$pValueMessage <- renderText({
+      if (all(p_values < 0.05)) {
+        "The p-values are good!"
+      } else {
+        "P-values are poor, try other variables for better significance."
+      }
+    })
+    
+    # Check if variance is good and display a message
+    output$varianceMessage <- renderText({
+      if (variance < 0.1) {
+        "Variance is good!"
+      } else {
+        "Variance is poor, check model fit."
+      }
     })
   })
 }
 
-
-# Run the application
 shinyApp(ui, server)
